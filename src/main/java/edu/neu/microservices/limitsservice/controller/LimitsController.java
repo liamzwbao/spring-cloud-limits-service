ef752ed3e15e4beb30e130cbff65b32c22c852ae
liamzwbao <zhiv.bao@gmail.com>
2023-01-08 16:14:28 -0800

    Get configuration from applications props

--- src/main/java/edu/neu/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/edu/neu/microservices/limitsservice/controller/LimitsController.java
@@ -1,13 +1,20 @@
 package edu.neu.microservices.limitsservice.controller;
 
 import edu.neu.microservices.limitsservice.bean.Limits;
+import edu.neu.microservices.limitsservice.configuration.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
+    private final Configuration configuration;
+
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping("/limits")
     public Limits retrieLimits() {
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
